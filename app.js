02cdfa3dcfd7a9b96307452487ced2ad04336439
Daniel Majewski <dm470876@wmservice.corpnet1.com>
2016-02-12 14:35:23 +0100

    jsut starting

--- app.js
+++ app.js
@@ -2,22 +2,22 @@ window.onload = function() {
     var canvas = document.getElementById('myCanvas');
     var ctx = canvas.getContext('2d');
 
-    ctx.beginPath();
-    ctx.rect(20, 40, 50, 50);
-    ctx.fillStyle = '#ff0000';
-    ctx.fill();
-    ctx.closePath();
+    var x = canvas.width/2;
+    var y = canvas.height-30;
+    var dx = 2;
+    var dy = -2;
+
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
+        ctx.arc(x, y, 10, 0, Math.PI*2);
+        ctx.fillStyle = '#0095dd';
+        ctx.fill();
+        ctx.closePath();
+        x += dx;
+        y += dy;
+    }
     
-    ctx.beginPath();
-    ctx.arc(200, 200, 20, 0, Math.PI * 2, false);
-    ctx.fillStyle = 'green';
-    ctx.fill();
-    ctx.closePath();
-    
-    ctx.beginPath();
-    ctx.rect(160, 10, 100, 40);
-    ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-    ctx.stroke();
-    ctx.closePath();
+    setInterval(draw, 10);
 }
 
